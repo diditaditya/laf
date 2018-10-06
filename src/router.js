c1e533932ea9112602cbc940252d8075b727a022
diditaditya <didit@Didits-MacBook-Pro.local>
2018-10-06 19:39:02 +0700

    remove options from route templating

--- src/router.js
+++ src/router.js
@@ -28,21 +28,6 @@ class Routes {
     }
   }
 
-  _createMiddlewares(method) {
-    return (req, res, next) => {
-      let tableName = req.params.tableName;
-      let mws = this._middlewares;
-      if (mws.default) {
-        if (mws.default.all) {
-          return mws.default.all;
-        }
-        if (mws.default[method]) {
-
-        }
-      }
-    }
-  }
-
   createStandardRoutes() {
 
     let controls = controller.createStandardControllers();
@@ -51,21 +36,10 @@ class Routes {
       let method = controls[act].method;
       let idRequired = method === "put" || method === "delete";
       path = idRequired ? `${path}/:id` : path;
-      let options = controls[act].options;
-      let mws = this._middlewares || null;
-      if (mws) {
-        if (mws.all) {
-          options = mws.all;
-        }
-        if (mws[method]) {
-          options = Object.assign(options, mws[method]);
-        }
-      }
       this._routes[act] = {
         path,
         method,
         main: controls[act].main,
-        options
       }
     }
   }
